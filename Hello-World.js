4415e5791b30b06c1bbe2c05161080b80f07128a
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2024-04-10 21:36:48 -0500

    Changes - Hello-World - Js

--- Hello-World.js
+++ Hello-World.js
@@ -1,5 +1,5 @@
 //* Hello World - JavaScript
 
-var text = "Hello World" // Variable text with a text "Hello World"
+var text = "Hello World" //? Variable text with a text "Hello World"
 
 console.log(text) // Print variable a with the text "Hello World"
